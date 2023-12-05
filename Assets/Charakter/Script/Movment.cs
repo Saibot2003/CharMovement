ff5d2b82681a87953e887e705f7cd73ebf56571e
Saibot2003 <schneidertobias187@gmail.com>
2023-12-05 16:54:54 +0100

    Tryed to Animate

--- Assets/Charakter/Script/Movment.cs
+++ Assets/Charakter/Script/Movment.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class PokemonMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-    public float rotationSpeed = 10f;
-
     private Rigidbody2D rb;
     private Animator animator;
+
+    // Variable für die zusätzliche Rotation
+    private Quaternion additionalRotation = Quaternion.identity;
 
     void Start()
     {
@@ -25,14 +26,21 @@ public class PokemonMovement : MonoBehaviour
         if (movement != Vector2.zero)
         {
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
-            rb.rotation = Mathf.LerpAngle(rb.rotation, angle, Time.deltaTime * rotationSpeed);
 
-            // Animationen steuern basierend auf der Richtung
-            UpdateAnimations(movement);
+            // Nur die Rotation aktualisieren, wenn sich die Richtung ändert
+            if (angle != 0)
+            {
+                // Setze die zusätzliche Rotation basierend auf der Himmelsrichtung
+                SetAdditionalRotation(angle);
+
+                // Animationen steuern basierend auf der Richtung
+                UpdateAnimations(movement);
+            }
         }
         else
         {
-            // Wenn keine Bewegung stattfindet, spiele die Idle-Animation
+            // Wenn keine Bewegung stattfindet, stoppe den Charakter und spiele die Idle-Animation
+            rb.velocity = Vector2.zero;
             animator.SetFloat("Speed", 0f);
         }
 
@@ -42,36 +50,58 @@ public class PokemonMovement : MonoBehaviour
 
     void UpdateAnimations(Vector2 movement)
     {
-        // Berechnung des Winkels
-        float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+        // Setze die entsprechende Richtungsanimation
+        animator.SetFloat("Speed", 1f);
 
         // Feststellen, in welche Himmelsrichtung sich der Charakter bewegt
+        if (movement.x > 0)
+        {
+            // Bewegung nach rechts
+            animator.Play("RightAnimation");
+        }
+        else if (movement.x < 0)
+        {
+            // Bewegung nach links
+            animator.Play("LeftAnimation");
+        }
+
+        if (movement.y > 0)
+        {
+            // Bewegung nach oben
+            animator.Play("UpAnimation");
+        }
+        else if (movement.y < 0)
+        {
+            // Bewegung nach unten
+            animator.Play("DownAnimation");
+        }
+    }
+
+    void SetAdditionalRotation(float angle)
+    {
+        // Setze die zusätzliche Rotation basierend auf der Himmelsrichtung
         if (angle > -45 && angle <= 45)
         {
             // Bewegung nach rechts
-            PlayAnimation("Right");
+            additionalRotation = Quaternion.Euler(0f, 0f, 0f);
         }
         else if (angle > 45 && angle <= 135)
         {
             // Bewegung nach oben
-            PlayAnimation("Up");
+            additionalRotation = Quaternion.Euler(0f, 0f, 0f);
         }
         else if (angle > 135 || angle <= -135)
         {
             // Bewegung nach links
-            PlayAnimation("Left");
+            additionalRotation = Quaternion.Euler(0f, 0f, 0f);
         }
         else if (angle > -135 && angle <= -45)
         {
             // Bewegung nach unten
-            PlayAnimation("Down");
+            additionalRotation = Quaternion.Euler(0f, 0f, 0);
         }
-    }
 
-    void PlayAnimation(string direction)
-    {
-        // Setze die entsprechende Richtungsanimation
-        animator.SetFloat("Speed", 1f);
-        animator.Play(direction + "Animation");
+        // Setze die zusätzliche Rotation für den Charakter
+        transform.rotation = additionalRotation;
     }
 }
